4535eabd738d6c460b87bb5aaf63eeeb0838f52a
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2023-02-17 22:45:45 +0530

    Update nextra config

--- nextra.config.js
+++ nextra.config.js
@@ -1,11 +1,6 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  images: {
-    "loader": "akamai",
-    "path": "",
-  },
-  trailingSlash: true,
 });
 
 module.exports = withNextra();
