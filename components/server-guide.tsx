707b1f0d2390b962a8ea1549b75fbd0a3d0aa897
Aayush <mahapatraaayush@gmail.com>
2023-06-09 12:45:18 +0530

    Create server hosting guide for compute (#40)
    * Add go docs
    
    * Add rust docs
    
    * Add ruby on rails docs
    
    * Add express docs

--- components/server-guide.tsx
+++ components/server-guide.tsx
@@ -1,9 +1,25 @@
 import FrameworkCard, { IFrameworkCard } from "./framework-card";
 import DjangoIcon from "../assets/django.svg";
 import NextIcon from "../assets/nextjs.svg";
+import RustIcon from "../assets/rust.svg";
+import GoIcon from "../assets/go.svg";
+import RailsIcon from "../assets/rails.svg";
+import ExpressIcon from "../assets/express.svg";
 import styles from "../styles/framework.module.css";
 
 const SERVER_LIST = [
+  {
+    path: "/server-guide/nextjs",
+    logo: NextIcon,
+    alt: "nextjs",
+    title: "Deploy Next App",
+  },
+  {
+    path: "/server-guide/rust",
+    logo: RustIcon,
+    alt: "rust",
+    title: "Deploy Rust App",
+  },
   {
     path: "/server-guide/django",
     logo: DjangoIcon,
@@ -11,10 +27,22 @@ const SERVER_LIST = [
     title: "Deploy Django App",
   },
   {
-    path: "/server-guide/nextjs",
-    logo: NextIcon,
-    alt: "nextjs",
-    title: "Deploy Next App",
+    path: "/server-guide/go",
+    logo: GoIcon,
+    alt: "go",
+    title: "Deploy Go App",
+  },
+  {
+    path: "/server-guide/rails",
+    logo: RailsIcon,
+    alt: "rails",
+    title: "Deploy Rails App",
+  },
+  {
+    path: "/server-guide/express",
+    logo: ExpressIcon,
+    alt: "express",
+    title: "Deploy Express App",
   },
 ];
 
---
+++ examples/goapp/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":8000", nil)
+}
