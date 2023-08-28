5b403580161d0e19bc63d48c4e963d399e4cabd9
Aayush <mahapatraaayush@gmail.com>
2023-08-28 19:41:53 +0530

    Add No Scaling Docs (#78)

--- components/scaling.tsx
+++ components/scaling.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/quick.module.css";
 
 const SCALING_LIST = [
   {
-    path: "/compute/scaling",
+    path: "/compute/scaling/no-scaling",
     emoji: "😢",
     title: "No Scaling",
     body: "Easy instance setup.",
