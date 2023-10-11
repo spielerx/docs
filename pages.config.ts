15e7049b45a8ba34c45b55d4ed8baaf626f50c32
Aayush <mahapatraaayush@gmail.com>
2023-10-11 09:18:19 +0530

    Add billing section under Storage (#103)
    * Add upload section in storage docs
    
    * Add upload sections under buckets
    
    * Add IPNS section under Buckets
    
    * Add ipns details on storage dashboard
    
    * Add dedicated gateways link in storage section
    
    * Add domains section under buckets
    
    * Add migration docs from <v2 to >v2
    
    * Add encryption and access control docs
    
    * Add browser upload sdk vs storage sdk
    
    * Add rate limits section under Storage
    
    * Add billing section under Storage
    
    * Resolve RCs
    
    * Add add-ons pricing
    
    * Update addons pricing
    
    * Update HNS and ENS
    
    * Add gateway bandwidth

--- pages.config.ts
+++ pages.config.ts
@@ -41,7 +41,6 @@ export const Pages = [
   ["Spot", "/compute/type/spot"],
   ["On Demand", "/compute/type/on-demand"],
   ["Autoscale", "/compute/type/autoscale"],
-  ["Plans", "/compute/plans"],
   ["Upload", "/storage/upload"],
   ["Dedicated Gateways", "/gateways/dedicated"],
   ["Public Gateway", "/gateways/public-gateway"],
