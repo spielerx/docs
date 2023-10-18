7dae1b29d21024401ae6f0c8b8d9e4fee21a1c5d
Aayush <mahapatraaayush@gmail.com>
2023-10-18 10:38:35 +0530

    Update billing section in docs (#107)
    * Update billing section in docs
    
    * Add new chains
    
    * Add credit card and Update payment methods
    
    * Update invoices in billing section
    
    * Remove coupon section from billing

--- components/upgrade.tsx
+++ components/upgrade.tsx
@@ -1,14 +1,25 @@
 import UpgradeCard, { IUpgradeCard } from "./upgrade-card";
+import CreditCardIcon from "../assets/credit-card.svg";
 import PolygonIcon from "../assets/polygon.svg";
 import OptimismIcon from "../assets/optimism.svg";
 import BscIcon from "../assets/bsc.svg";
 import GnosisIcon from "../assets/gnosis.svg";
 import ArbitrumIcon from "../assets/arbitrum.svg";
 import AvalancheIcon from "../assets/avalanche.svg";
+import FilecoinIcon from "../assets/filecoin.svg";
+import MantleIcon from "../assets/mantle.svg";
+import BaseIcon from "../assets/base.svg";
+import LineaIcon from "../assets/linea.svg";
 import SolanaIcon from "../assets/solana.svg";
 import styles from "../styles/upgrade.module.css";
 
 const UPGRADE_LIST = [
+  {
+    path: "/billing/upgrade/credit-card",
+    logo: CreditCardIcon,
+    alt: "credit-card",
+    title: "Upgrade to Pro with Credit Card",
+  },
   {
     path: "/billing/upgrade/polygon",
     logo: PolygonIcon,
@@ -44,6 +55,30 @@ const UPGRADE_LIST = [
     logo: AvalancheIcon,
     alt: "avalanche",
     title: "Upgrade to Pro with Avalanche",
+  },
+  {
+    path: "/billing/upgrade/filecoin",
+    logo: FilecoinIcon,
+    alt: "filecoin",
+    title: "Upgrade to Pro with Filecoin",
+  },
+  {
+    path: "/billing/upgrade/mantle",
+    logo: MantleIcon,
+    alt: "mantle",
+    title: "Upgrade to Pro with Mantle",
+  },
+  {
+    path: "/billing/upgrade/base",
+    logo: BaseIcon,
+    alt: "base",
+    title: "Upgrade to Pro with Base",
+  },
+  {
+    path: "/billing/upgrade/linea",
+    logo: LineaIcon,
+    alt: "linea",
+    title: "Upgrade to Pro with Linea",
   },
   {
     path: "/billing/upgrade/solana",
