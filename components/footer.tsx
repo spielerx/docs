2b287cec53e37029e2627192c4a2f2fad351c880
Abhinav Gaur <117283283+abhinav-spheron@users.noreply.github.com>
2024-06-20 20:04:54 +0530

    Remove status site from footer (#171)
    * Remove status site from footer
    
    * Footer justify center

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,55 +1,55 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Image from "next/image";
 import Spheron from "../assets/spheron.svg";
 import styles from "../styles/footer.module.css";
 
-interface IStatus {
-  indicator: string;
-  description: string;
-}
+// interface IStatus {
+//   indicator: string;
+//   description: string;
+// }
 
 const Footer = () => {
-  const [status, setStatus] = useState<IStatus>({
-    indicator: "none",
-    description: "All Systems Operational",
-  });
-  const [colorCode, setColorCode] = useState("#0057FF");
+  // const [status, setStatus] = useState<IStatus>({
+  //   indicator: "none",
+  //   description: "All Systems Operational",
+  // });
+  // const [colorCode, setColorCode] = useState("#0057FF");
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch(
-          "https://status.spheron.network/api/v2/status.json"
-        );
-        if (!response.ok) throw new Error("Network response was not ok");
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const response = await fetch(
+  //         "https://status.spheron.network/api/v2/status.json"
+  //       );
+  //       if (!response.ok) throw new Error("Network response was not ok");
 
-        const colorMapping = {
-          none: "#0057FF",
-          minor: "#EFBF14",
-          major: "#DE0B0B",
-          critical: "#DE0B0B",
-        };
+  //       const colorMapping = {
+  //         none: "#0057FF",
+  //         minor: "#EFBF14",
+  //         major: "#DE0B0B",
+  //         critical: "#DE0B0B",
+  //       };
 
-        const result = await response.json();
-        setStatus(result.status);
-        setColorCode(colorMapping[result.status.indicator]);
-      } catch (error) {
-        setStatus({ indicator: "major", description: "Error" });
-        setColorCode("#DE0B0B");
-      }
-    })();
-  }, []);
+  //       const result = await response.json();
+  //       setStatus(result.status);
+  //       setColorCode(colorMapping[result.status.indicator]);
+  //     } catch (error) {
+  //       setStatus({ indicator: "major", description: "Error" });
+  //       setColorCode("#DE0B0B");
+  //     }
+  //   })();
+  // }, []);
 
   return (
     <section className={styles.footer}>
-      <a href="https://status.spheron.network/" className={styles.link}>
+      {/* <a href="https://status.spheron.network/" className={styles.link}>
         <div style={{ backgroundColor: colorCode }} className={styles.dot}>
           {" "}
         </div>
         <div style={{ fontSize: "12px", color: colorCode }}>
           {status.description}
         </div>
-      </a>
+      </a> */}
       <div
         className="nx-flex nx-items-center"
         style={{ marginLeft: "8px", marginBottom: "4px" }}
